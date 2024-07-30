3280ceede9c0b62998ab8ba3aecfe979f7722eb7
Badal3000 <120331977+Badal3000@users.noreply.github.com>
2024-07-30 12:24:40 +0000

    numbers and maths in javascript

---
+++ javascript/numbers_and_maths.js
@@ -0,0 +1,12 @@
+console.log(Math)
+//llibraries used in maths
+//Math.abs()->for the absolute values
+console.log(Math.abs(-10))
+console.log(Math.round(100.5)) // used for round off the value
+console.log(Math.ceil(100.01)) //used for the ceiling value(higher value)
+console.log(Math.floor(100.99)) // used for the floor value(lower value)
+console.log(Math.min(8, 9 , 3, 4)) // calculates the minimmum value
+console.log(Math.max(4,9,5,3,1,)) // calcullates the maximum value
+console.log(Math.random()) // gives the random value between 0 to onr but we can manipulate it within the ranges by multiplying between the values of multiples of 10  and applying floor value so it doesnt go out of bound
+console.log(Math.random()*100) // gives the random value between 0 to onr but we can manipulate it within the ranges by multiplying between the values of multiples of 10  and applying floor value so it doesnt go out of bound
+console.log(Math.floor(Math.random()*100)) // gives the random value between 0 to onr but we can manipulate it within the ranges by multiplying between the values of multiples of 10  and applying floor value so it doesnt go out of bound
