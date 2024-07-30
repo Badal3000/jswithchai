b47e9506d69c214194773f8ac1a7b1bd14a8f937
Badal3000 <120331977+Badal3000@users.noreply.github.com>
2024-07-30 09:19:52 +0000

    lecture 4 of javascript

---
+++ javascript/04comaparators.js
@@ -0,0 +1,19 @@
+//there are basically 7 comaparators
+/*
+1. < : less than
+2. >: greater than
+3. == equalto eaqual to
+4. <= :lesss than or equal to
+5. >=: greater than or EQUAL TO
+6 != : not eqUAL TO
+7. === : strict check if equal to  checks both values as  well as datatypes
+*/
+console.log("2" > 1);
+console.log("1">=2);
+console.log("2"===2);
+
+console.log(null>0);
+console.log(null>=0)
+console.log(null==0)
+console.log(null<0)
+console.log(null<=0)
