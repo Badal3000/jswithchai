87c9279a0cdfb623cfcc6d518454dcdd2c5dacda
Badal3000 <badal.chaurasia1234@gmail.com>
2024-10-06 16:14:16 +0000

    scope

---
+++ javascript/scope.js
@@ -0,0 +1,12 @@
+function  one(){
+    const username = "Badal"
+    function two(){
+        const website = "youtube"
+        console.log(uername);
+    }
+    //console.log(website);
+    two()
+
+}
+one()
+console.log(one())
